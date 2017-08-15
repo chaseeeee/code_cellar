90ddb4be9d01ef744d6686b62055bdd5e98f32fc
chasehammock <chasehammock@gmail.com>
2017-08-15 17:17:20 -0500

    added the conditionals.js

---
+++ js/conditionals.js
@@ -0,0 +1,15 @@
+'use strict';
+
+consile.log ('before the if statement');
+
+var numberOfTimesVoted = 1;
+
+if (false) {
+    console.log(body of the if statesment);
+
+} else if () {
+    console.log (' condition if evaluated to be false');
+    console.log ('else branch');
+}
+
+console.log ('after the if statement');
